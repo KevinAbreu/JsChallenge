ef34cc1ef1599c4245a38a21ef7d97c9071f05b5
Kevin Abreu <kevinrabreu@hotmail.com>
2019-04-30 21:12:52 -0400

    added a for loop to add arrays

---
+++ main.js
@@ -0,0 +1,12 @@
+// sum part
+var prices = [33,26,99,120,12,45];
+var sum = 0;
+for (sum = 0; sum < prices.length;) {
+  sum= prices[0] + prices[1] + prices[2] + prices[3] + prices[4] + prices[5];
+}
+console.log("The sum is:", sum);
+console.log("to be honest, I felt like I could have done do a bit better")
+document.write("The sum is:", sum);
+//end of sum part
+
+// time for a song!
